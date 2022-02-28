2ef80a254c054149421966b1aa29d07396f46b5e
Isaac Cidreira <isaaccidreira@hotmail.com>
2022-02-28 13:40:44 -0300

    desafio finalizado

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { response } from 'express';
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('Mensagem do erro');
+    }
+    const user = this.usersRepository.create({ email, name });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
